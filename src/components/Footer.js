ac17f05f52630016d727daf71a08edaf0e44e2d5
Anshulkumarsharma2804 <anshulsharma2804@gmail.com>
2024-09-06 18:45:24 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+const Footer=()=> {
+  return (
+  <footer className='footer'>
+   Copyright &#169; 2024 Anshul Sharma. All Right Reserved
+
+  </footer>
+  )
+}
+
+export default Footer
